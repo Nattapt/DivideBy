782ee1fda01c6f7d6b76424cfb33d14809f15807
Nattapat <san.1234@hotmail.com>
2018-10-07 18:53:47 +0700

    Initial Commit

---
+++ DivideBy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DivideBy
+//
+//  Created by Nattapat on 7/10/2561 BE.
+//  Copyright © 2561 Nattapat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
