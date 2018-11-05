3ed94800121b846096ab055d6f78585bc675e2da
Nattapat <san.1234@hotmail.com>
2018-11-05 17:39:44 +0700

    Layout

--- DivideBy/ViewController.swift
+++ DivideBy/ViewController.swift
@@ -10,28 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var showRandom: UILabel!
-    
-    var showRandom = "0000000000"
-    
-    showRandom.text = "\(showRandom)"
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func Button2(_ sender: UIButton) {
-        if showRandom % 2 {
-            Button2.colortext = green
-        } else {
-            hidesBottomBarWhenPushed
-    }
-    
 
 }
 
